f8a48b924e96301cd64c60a3028f65980569bb90
LeoDiaz225 <leodiaz225@outlook.com>
2025-07-07 10:05:43 -0300

    adv app2

--- app2-juego/app2/src/App.jsx
+++ app2-juego/app2/src/App.jsx
@@ -1,12 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  return (
+    <div className={className}>
+      {children}
+      </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const updateBoard = () => {
+   
+  };
 
   return (
-    <h1>Tic Tac Game</h1>
-  )
+    <main className="board">
+      <h1>Tic Tac Game</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square 
+            key={index} 
+            index={index}
+            updateBoard={updateBoard}
+            >
+              
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+        </Square>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
