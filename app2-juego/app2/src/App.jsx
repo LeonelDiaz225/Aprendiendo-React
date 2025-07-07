cd16afd125fa8e4b7705a9406c95c08cfd342bce
LeoDiaz225 <leodiaz225@outlook.com>
2025-07-07 14:38:38 -0300

    Combo winner

--- app2-juego/app2/src/App.jsx
+++ app2-juego/app2/src/App.jsx
@@ -9,41 +9,68 @@ const TURNS = {
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`;
-  return (
-    <div className={className}>
+  const handleClick = () => {
+    updateBoard(index); // Pasar el index aquí
+  };
+
+  return ( 
+    <div onClick={handleClick} className={className}> {/* Usar handleClick en lugar de updateBoard directamente */}
       {children}
-      </div>
+    </div>
   );
 };
+
+const winnerCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
-  const updateBoard = () => {
-   
+  const [winner, setWinner] = useState(null);
+
+  const updateBoard = (index) => {
+    // Verificar que la casilla esté vacía
+    if (board[index]) return;
+    
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
   };
 
   return (
     <main className="board">
       <h1>Tic Tac Game</h1>
       <section className="game">
-        {board.map((_, index) => {
+        {board.map((square, index) => {
           return (
             <Square 
-            key={index} 
-            index={index}
-            updateBoard={updateBoard}
+              key={index} 
+              index={index}
+              updateBoard={updateBoard}
             >
-              
+              {square} {/* Mostrar el contenido del tablero (X o O) */}
             </Square>
           );
         })}
       </section>
       <section className="turn">
         <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
         </Square>
         <Square isSelected={turn === TURNS.O}>
-        </Square>
+          {TURNS.O}
+        </Square> 
       </section>
     </main>
   );
