aa41dd73461ef23f57cc7a3b6e6750dd0fb6d2ce
LeoDiaz225 <leodiaz225@outlook.com>
2025-07-09 20:59:59 -0300

    config

--- app2-juego/app2/src/App.jsx
+++ app2-juego/app2/src/App.jsx
@@ -1,48 +1,46 @@
 import { useState } from "react";
 import "./App.css";
 import { Square } from "./components/Square";
-import { TURNS, winnerCombo } from "./constants";
+import { TURNS } from "./constants";
 import { checkWinnerFrom } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
   const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board')
-    if (boardFromStorage) 
-      return JSON.parse(boardFromStorage)
-      return Array(9).fill(null);
+    const boardFromStorage = window.localStorage.getItem("board");
+    if (boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
   });
 
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null);
- 
 
-   const resetGame = () => {
+  const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
-    window.localStorage.removeItem('board');
-    window.localStorage.removeItem('turn');
-    console.log('Juego reiniciado');
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
+    console.log("Juego reiniciado");
   };
 
   const isBoardFull = (newBoard) => {
     return newBoard.every((square) => square !== null);
-  }
-    
+  };
+
   const updateBoard = (index) => {
     // Verificar que la casilla esté vacía
     if (board[index] || winner) return;
-    
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
-    
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
-    window.localStorage.setItem('board', JSON.stringify(newBoard));
-    window.localStorage.setItem('turn', newTurn);
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
 
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
@@ -59,26 +57,18 @@ function App() {
       <section className="game">
         {board.map((square, index) => {
           return (
-            <Square 
-              key={index} 
-              index={index}
-              updateBoard={updateBoard}
-            >
+            <Square key={index} index={index} updateBoard={updateBoard}>
               {square} {/* Mostrar el contenido del tablero (X o O) */}
             </Square>
           );
         })}
       </section>
       <section className="turn">
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square> 
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
-      <WinnerModal resetGame={resetGame} winner={winner}/>
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   );
 }
